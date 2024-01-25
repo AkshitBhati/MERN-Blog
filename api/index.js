cf5676e09905dad4c013cf5e68490f62b0f25161
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-01-25 22:44:24 +0530

    creating the backend structure

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./connectDb.js"
+import UserRoutes from "./routes/user.route.js"
 
 const app = express()
 
 dotenv.config()
 
 connectDb()
+
+app.use("/api",UserRoutes)
+
+// app.get("/test", (req, res) => {
+//     res.json({message:"API is working"})
+// })
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
