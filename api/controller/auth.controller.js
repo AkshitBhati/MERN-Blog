f3dec400d5ed1515875fba8c08e3c112e2519d7f
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-01-29 21:41:48 +0530

    created signin api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../modals/user.modal.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 const signup = async (req, res, next) => {
     try {
@@ -24,4 +25,36 @@ const signup = async (req, res, next) => {
     }
 }
 
-export { signup }
+const signin = async(req, res, next) => {
+    const { email, password } = req.body
+    if(!email || !password || email === "" || password === ""){
+        next(errorHandler(400, 'All feilds are required'))
+    }
+    
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            next(errorHandler(404, "User not found"))
+        } 
+
+        const validPassword = bcryptjs.compareSync(password, validUser.hashPassword)
+        if(!validPassword){
+            return next(errorHandler(400, "Invalid Password"))
+            
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.jwtsecret);
+
+        res.cookie('access-token', token, {
+            httpOnly: true,
+        });
+
+        const { hashPassword:pass, ...rest } = validUser._doc
+        res.status(200).json(rest);
+    } catch (err) {
+        next(err);
+    }
+        
+}
+
+export { signup, signin }
