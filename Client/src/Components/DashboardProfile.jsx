ad17846c768bc89234ac0cd8ba941c380d16ef99
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-02-01 22:23:23 +0530

    complte profile page UI

--- Client/src/Components/DashboardProfile.jsx
+++ Client/src/Components/DashboardProfile.jsx
@@ -1,9 +1,20 @@
+import { TextInput } from 'flowbite-react'
 import React from 'react'
+import { useSelector } from "react-redux"
 
 const DashboardProfile = () => {
+    const { currentUser } = useSelector((state) => state.user)
   return (
-    <div>
-      Profile
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <img src={currentUser.profilePicture} alt="User" className='rounded-full w-full h-full border-8 object-cover border-[lightgray]'/>
+        </div>
+        <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username}/>
+        <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email}/>
+        <TextInput type='password' id='password' placeholder='Password' />
+      </form>
     </div>
   )
 }
