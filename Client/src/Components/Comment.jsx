6e61de3b2dfe489a602a1139da1805f4d9d70f8e
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-02-20 14:28:05 +0530

    add like functionality to the comment component

--- Client/src/Components/Comment.jsx
+++ Client/src/Components/Comment.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import moment from "moment"
+import { FaThumbsUp } from "react-icons/fa"
+import { useSelector } from "react-redux";
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, onLike }) => {
+  const { currentUser } = useSelector((state) => state.user)
   const [user, setUser] = useState({});
   useEffect(() => {
     const getUser = async () => {
@@ -18,7 +21,7 @@ const Comment = ({ comment }) => {
     };
     getUser();
   }, []);
-  console.log(user);
+  
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
       <div className="flex-shrink-0 mr-3">
@@ -34,6 +37,12 @@ const Comment = ({ comment }) => {
             <span className="text-gray-500 text-xs">{moment(comment.createdAt).fromNow()}</span>
         </div>
         <p className="text-gray-500 mb-2">{comment.content}</p>
+        <div className="flex items-center gap-2 pt-2 text-xs border-t dark:border-gray-700 max-w-fit ">
+          <button type="button" onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}>
+            <FaThumbsUp className="text-sm "/>
+          </button>
+          <p>{comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes===1 ?"like" : "likes")}</p>
+        </div>
       </div>
     </div>
   );
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js" 
-import { create, getPost, deletepost } from "../controller/post.controller.js"
+import { create, getPost, deletepost, updatepost } from "../controller/post.controller.js"
 
 const router = express.Router()
 
 router.post('/create', verifyToken, create)
 router.get('/getposts', getPost)
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
-
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 export default router
