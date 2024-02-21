5a1c20c0924f7c8f120b4a085be8abbf4b56737a
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-02-21 12:48:31 +0530

    some changes for deployment

--- api/index.js
+++ api/index.js
@@ -24,11 +24,11 @@ app.use("/api/auth",authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commetRoutes)
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 //middleware
 app.use((err, req, res, next) => {
